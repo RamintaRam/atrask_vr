81414ea3f386976f5a9d914ed4f316367e1369f5
RamintaRam <raminta5@yahoo.com>
2017-06-07 11:35:05 +0300

    Middleware created - Check if Admin

---
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckIfAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(in_array("super-admin", auth()->user()->rolesConnections()->pluck('roles_id')->toArray()))
+        return $next($request);
+
+        return redirect('home');
+
+    }
+}
