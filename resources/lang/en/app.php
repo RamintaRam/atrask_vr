e672bc2c4ac0eb038f07baaa39823495bb25dc8e
RamintaRam <raminta5@yahoo.com>
2017-06-08 13:35:58 +0300

    lan folder: lt and en (app)

---
+++ resources/lang/en/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+
+    'language' => 'Language',
+    'menu' => 'Menu',
+    'pages' => "Pages",
+    'orders' => "Orders",
+    'categories' => "Categories",
+    'users' => "Users",
+    'admin' => 'Admin account',
+];
---
+++ resources/lang/lt/app.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+
+    'language' => 'Kalba',
+    'menu' => 'Menu',
+    'pages' => "Puslapiai",
+    'orders' => "Užsakymai",
+    'categories' => "Kategorijos",
+    'users' => "Vartotojai",
+    'admin' => 'Administratoriaus paskyra',
+];
