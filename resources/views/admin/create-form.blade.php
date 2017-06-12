07751d0b3fba17a61ede419e83a8a72f834f60d6
RamintaRam <raminta5@yahoo.com>
2017-06-12 15:38:15 +0300

    create form updated

--- resources/views/admin/create-form.blade.php
+++ resources/views/admin/create-form.blade.php
@@ -5,9 +5,25 @@
     <div id="list">
 
         <div class="container">
-            <div><h2>{{trans('app.create' . 'app.' . $tableName)}}</h2></div>
+            <div><h2>{{trans('app.' . $tableName)}}</h2></div>
+
+            {!! Form::open(['url' => route('app.' . $tableName . '.store')]) !!}
+
+            @foreach($fields as $field)
+
+                @if($field['type'] == 'dropDown')
+                    {!! Form::label(trans('app.' . $field['key'])) !!}<br/>
+                    {{Form::select($field['key'], $field['option'])}}<br/><br/>
+                @elseif($field['type'] == 'singleLine')
+                    {!! Form::label(trans('app.' . $field['key'])) !!}<br/>
+                    {!! Form::text($field['key'])!!}<br/><br/><br/>
 
 
+                @endif
+            @endforeach
+
+            {!! Form::submit(trans('app.create') , ['class' => 'btn btn-success']) !!}
+            <a class="btn btn-primary" href="{{ route('app.' . $tableName . '.index') }}">{{trans('app.' . $tableName)}}</a>
 
         </div>
     </div>
@@ -15,4 +31,4 @@
 
 
 
-@endsection
+  @endsection
