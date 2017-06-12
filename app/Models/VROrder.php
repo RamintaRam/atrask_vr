3976b8f7391db13227bc8fa2349e4d0caa7e0a45
RamintaRam <raminta5@yahoo.com>
2017-06-12 10:18:35 +0300

    used trait for translations

--- app/Models/VROrder.php
+++ app/Models/VROrder.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use App\Traits\TableNameTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class VROrder extends Model
 {
+    use TableNameTrait;
+
     protected $table = 'vr_order';
 
     protected $fillable = ['id', 'status', 'user_id'];
