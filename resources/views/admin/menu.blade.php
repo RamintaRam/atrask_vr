f61e6df2a127572ed6ff402ebb87c664aa8e1861
RamintaRam <raminta5@yahoo.com>
2017-06-12 14:07:04 +0300

    routes in main blade fixed

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,15 +1,14 @@
 <div id="menu">
-<h3 class="admin">{{trans('app.admin')}}</h3><br>
-        <dl>
-        <dt><a href="{{'app.language.index'}}">{{trans('app.language')}}</a></dt>
-            <dt> <a href="{{'app.menu.index'}}">{{trans('app.menu')}}</a></dt>
-            <dt> <a href="{{'app.pages.index'}}">{{trans('app.pages')}}</a></dt>
-            <dt> <a href="{{'app.orders.index'}}">{{trans('app.orders')}}</a></dt>
-            <dt> <a href="{{'app.users.index'}}">{{trans('app.users')}}</a></dt>
-            <dt> <a href="{{'app.categories.index'}}">{{trans('app.categories')}}</a></dt>
+    <h3 class="admin">{{trans('app.admin')}}</h3><br>
+    <dl>
+        <dt><a href="{{route('app.language.index')}}">{{trans('app.language')}}</a></dt>
+        <dt><a href="{{route('app.menu.index')}}">{{trans('app.menu')}}</a></dt>
+        <dt><a href="{{route('app.pages.index')}}">{{trans('app.pages')}}</a></dt>
+        <dt><a href="{{route('app.orders.index')}}">{{trans('app.orders')}}</a></dt>
+        <dt><a href="{{route('app.users.index')}}">{{trans('app.users')}}</a></dt>
+        <dt><a href="{{route('app.categories.index')}}">{{trans('app.categories')}}</a></dt>
 
 
-
-        </dl>
+    </dl>
 
 </div>
