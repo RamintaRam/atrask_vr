dace7c11bc9c7dcdc43e68e5a7c584d2b04eec57
RamintaRam <raminta5@yahoo.com>
2017-06-13 11:53:13 +0300

    controller updated

--- app/Http/Controllers/VRCategoriesController.php
+++ app/Http/Controllers/VRCategoriesController.php
@@ -5,65 +5,61 @@ use App\VRCategoriesTranslations;
 use App\VRLanguageCodes;
 use Illuminate\Routing\Controller;
 
-class VRCategoriesController extends Controller {
+class VRCategoriesController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /vrcategories
-	 *
-	 * @return Response
-	 */
-	public function adminIndex()
-	{
+    /**
+     * Display a listing of the resource.
+     * GET /vrcategories
+     *
+     * @return Response
+     */
+    public function adminIndex()
+    {
         $dataFromModel = new VRCategories();
         $config['tableName'] = $dataFromModel->getTableName();
         $config['list'] = VRCategories::get()->toArray();
         $config['new'] = 'app.categories.create';
 
         return view('admin.list', $config);
-	}
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /vrcategories/create
+    /**
+     * Show the form for creating a new resource.
+     * GET /vrcategories/create
      *
      * @return Response
      */
     public function adminCreate()
     {
         $config = $this->getFormData();
-        $data = request()->all();
         $dataFromModel = new VRCategories();
         $config['tableName'] = $dataFromModel->getTableName();
         $config['list'] = VRCategories::get()->toArray();
         $config['route'] = route('app.categories.create');
 
-//getFormData() funkcija apraryta apacioje.
-
-
-
-
+        //getFormData() funkcija apraryta apacioje.
 
         return view('admin.create-form', $config);
     }
 
     /**
-	 * Store a newly created resource in storage.
-	 * POST /vrcategories
-	 *
-	 * @return Response
-	 */
+     * Store a newly created resource in storage.
+     * POST /vrcategories
+     *
+     * @return Response
+     */
     public function adminStore()
     {
         $data = request()->all();
-/*
- * VIENAS BUDAS:
-        $record = VRCategories::create();
-        VRCategoriesTranslations::create([
-            'record_id' => $record->id,
-            'language_code' => $data['language_code'],
-            'name' => $data['name'],
-        ]);*/
+        /*
+         * VIENAS BUDAS:
+                $record = VRCategories::create();
+                VRCategoriesTranslations::create([
+                    'record_id' => $record->id,
+                    'language_code' => $data['language_code'],
+                    'name' => $data['name'],
+                ]);*/
 
 //ANTRAS BUDAS:
         $record = VRCategories::create();
@@ -82,67 +78,67 @@ class VRCategoriesController extends Controller {
 
     /**
      * Display the specified resource.
-	 * GET /vrcategories/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+     * GET /vrcategories/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /vrcategories/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function adminEdit($id)
-	{
+    /**
+     * Show the form for editing the specified resource.
+     * GET /vrcategories/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function adminEdit($id)
+    {
         $config = $this->getFormData();
         $dataFromModel = new VRCategories();
         $config['tableName'] = $dataFromModel->getTableName();
         $config['route'] = route('app.categories.edit', $id);
-
+        $record = VRCategories::find($id)->toArray();
 
         return view('admin.create-form', $config);
-	}
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /vrcategories/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     * PUT /vrcategories/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /vrcategories/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /vrcategories/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 
-	public function getFormData()
+    public function getFormData()
     {
         $config['fields'][] = [
             "type" => "dropDown",
             "key" => "language_code",
             "option" => getActiveLanguages(),
         ];
-        $config['fields'][]=
+        $config['fields'][] =
             [
                 "type" => "singleLine",
                 "key" => "name",
