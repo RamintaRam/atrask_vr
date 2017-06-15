596d4305f4b7bd64a5a05b94466cc5211dfe9991
RamintaRam <raminta5@yahoo.com>
2017-06-15 10:42:08 +0300

    updated helper function

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -5,18 +5,20 @@ use App\VRLanguageCodes;
 
 function getActiveLanguages()
 {
-    $locale = app()->getLocale();
     $config = VRLanguageCodes::where('is_active', 1)->pluck('name', 'id')->toArray();
+    $locale = app()->getLocale();
+
+    if (!isset($config[$locale]))
+    {
+        $locale = config('app.fallback_locale');
+
+        if (!isset($config[$locale]))
+        {
+            return $config;
+        }
+    }
 
     $config = array($locale => $config[$locale]) + $config;
-
-        return $config;
-
-
-//        if($locale )
-
-//    dd($config);
-
-
+    return $config;
 }
 
