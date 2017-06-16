5900d3515d1deacc32170331629ecba664bd07d2
RamintaRam <raminta5@yahoo.com>
2017-06-16 13:36:52 +0300

    updated menuTranslations

--- app/Models/VRMenuTranslations.php
+++ app/Models/VRMenuTranslations.php
@@ -10,20 +10,4 @@ class VRMenuTranslations extends CoreModel
     protected $table = 'vr_menu_translations';
 
     protected $fillable = ['id', 'url', 'name', 'menu_id', 'language_code', 'record_id'];
-
-
-    protected static function boot() {
-        parent::boot();
-        static::creating(function($model) {
-            if(!isset($model->attributes['id'])) {
-                $model->attributes['id'] = Uuid::uuid4();
-            } else {
-                $model->{$model->getKeyName()} = $model->attributes['id'];
-            }
-        });
-
-
-    }
-
-
 }
